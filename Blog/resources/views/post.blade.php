4eb95393c9c5cc0227494224ebc46de2e5fe2626
Christine Goodrich <christea.anna@gmail.com>
2022-03-14 05:37:22 -0400

    I added more routes to my app

---
+++ Blog/resources/views/post.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<title> My Blog </title>
+<link rel = "stylesheet" href="/app.css">
+
+<!--<header>
+    <strong>
+        Christine's Portfolio
+        <nav class = "flex justify-between items-center">
+            <ul>
+                <h1> Blog </h1>
+
+                <h1> Analytics </h1>
+
+                <h1>Software</h1>
+
+                <h1>Research</h1>
+
+                <h1>Hobbies</h1>
+            </ul>
+        </nav>
+    </strong>
+</header>-->
+
+
+<body>
+<article>
+    <h1><a href = "/post">My First Post</a></h1>
+    <p>She sells sea shells on the sea shore, the shells she sells are sea shells I'm sure, so if she sells sea shells on the sea shore, I'm sure she sells sea shore shells.</p>
+</article>
+</body>
---
+++ Blog/resources/views/posts.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<title> My Blog </title>
+<link rel = "stylesheet" href="/app.css">
+
+<!--<header>
+    <strong>
+        Christine's Portfolio
+        <nav class = "flex justify-between items-center">
+            <ul>
+                <h1> Blog </h1>
+
+                <h1> Analytics </h1>
+
+                <h1>Software</h1>
+
+                <h1>Research</h1>
+
+                <h1>Hobbies</h1>
+            </ul>
+        </nav>
+    </strong>
+</header>-->
+
+
+<body>
+<article>
+    <h1><a href = "/post">My First Post</a></h1>
+    <p>She sells sea shells on the sea shore, the shells she sells are sea shells I'm sure, so if she sells sea shells on the sea shore, I'm sure she sells sea shore shells.</p>
+</article>
+
+<article>
+    <h1><a href = "/post">My Second Post</a></h1>
+    <p>She sells other shells on the sea shore, the shells she sells are other shells I'm sure, so if she sells other shells on the sea shore, I'm sure she sells other shore shells.</p>
+</article>
+
+<article>
+    <h1><a href = "/post">My Third Post</a></h1>
+    <p>She sells other shells on the sea shore, the shells she sells are other shells I'm sure, so if she sells other shells on the sea shore, I'm sure she sells other shore shells.</p>
+</article>
+</body>
+
